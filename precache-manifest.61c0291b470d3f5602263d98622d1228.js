51129189e66be451a5e694eee61c61f00e2b274b
unknown <steca93@gmail.com>
2020-03-21 00:57:23 +0100

    Updates

---
+++ precache-manifest.61c0291b470d3f5602263d98622d1228.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b67c55b1d6a17480856165711ae4d67d",
+    "url": "/monsters-rolodex-steca2/index.html"
+  },
+  {
+    "revision": "0c8e6f935df14b7a5aef",
+    "url": "/monsters-rolodex-steca2/static/css/main.62b45751.chunk.css"
+  },
+  {
+    "revision": "4ecb9f0df0474c1b1fa9",
+    "url": "/monsters-rolodex-steca2/static/js/2.47062208.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex-steca2/static/js/2.47062208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c8e6f935df14b7a5aef",
+    "url": "/monsters-rolodex-steca2/static/js/main.391d49e3.chunk.js"
+  },
+  {
+    "revision": "76137383d493dfa38476",
+    "url": "/monsters-rolodex-steca2/static/js/runtime-main.31d78eea.js"
+  }
+]);
